936d0eee000e67747efbf99b1c2f946beed295b3
maurya20 <mukesh.ice17@gmail.com>
2021-02-14 08:53:21 +0530

    image middleware implemented in routes

--- src/routes/Products.ts
+++ src/routes/Products.ts
@@ -1,36 +1,9 @@
 import express, {Request,Response,NextFunction} from 'express';
 import {ProductModel} from '../models/ProductsModel';
-import multer from 'multer';
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads');
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-//@ts-ignore
-const fileFilter = (req, file, cb) => {
-  // reject a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
-
+import {imageMiddleware} from "../middleware/imageMiddleware"
 ///
-router.post("/product", upload.single('photo'), (req, res, next) => {
+router.post("/product", imageMiddleware.single('image'), (req, res, next) => {
   const newproduct = new ProductModel({
     name: req.body.name,
     price: req.body.price,
